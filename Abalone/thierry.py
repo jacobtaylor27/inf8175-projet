b93dd1cf286aa6a9344991c7fdca1ac4436dc87c
Laurie Bédard-Côté <laurie.bedard-cote@polymtl.ca>
2024-04-17 15:59:40 -0400

    thierry

--- Abalone/thierry.py
+++ Abalone/thierry.py
@@ -1,139 +1,11 @@
-import copy
+import math
+
 from player_abalone import PlayerAbalone
 from game_state_abalone import GameStateAbalone
 from seahorse.game.action import Action
 from seahorse.game.game_state import GameState
-from board_abalone import BoardAbalone
 from seahorse.utils.custom_exceptions import MethodNotImplementedError
 
-from math import inf
-
-
-# ----------- Utils functions ------------ #
-def manhattanDist(A: list[int], B: list[int]) -> int:
-    """
-    Compute the Manhattan distance between two points.
-
-    Args:
-        A (list[int]): First point
-        B (list[int]): Second point 
-    
-    Returns:
-        int: Manhattan distance between the two points
-    """
-    mask1 = [(0,2),(1,3),(2,4)]
-    mask2 = [(0,4)]
-    diff = (abs(B[0] - A[0]),abs(B[1] - A[1]))
-    dist = (abs(B[0] - A[0]) + abs(B[1] - A[1]))/2
-    if diff in mask1:
-        dist += 1
-    if diff in mask2:
-        dist += 2
-    return dist
-
-def get_opponent(state: GameStateAbalone, player: PlayerAbalone,) -> PlayerAbalone:
-    players = state.get_players()
-    return players[1] if players[0] == player else players[0]
-    
-
-# ----------- Heuristic function ------------ #
-def get_score_difference(state: GameStateAbalone, player: PlayerAbalone, scores: dict[int, float]) -> int:
-    heuristic = 0
-    opponent = get_opponent(state, player)
-
-    previous_player_score = scores[player.get_id()]
-    previous_opponent_score = scores[opponent.get_id()]
-
-    player_score = state.scores[player.get_id()]
-    opponent_player_score = state.scores[opponent.get_id()]
-
-    if player_score < previous_player_score:
-        heuristic -= 100000
-    if opponent_player_score < previous_opponent_score:
-        heuristic += 250000
-        
-    return heuristic
-
-def get_center_proximity(state: GameStateAbalone, player: PlayerAbalone) -> int:
-    center = (8,4)
-    env = state.get_rep().get_env()
-    dist = 0
-    nb_p = 0
-
-    for p in list(env.items()):
-        key, value = p
-        if value.get_owner_id() == player.get_id():
-            dist += manhattanDist(center, key)
-            nb_p += 1
-  
-    return dist/nb_p
-
-
-def heuristic(state: GameStateAbalone, player: PlayerAbalone, scores: dict[int, float]) -> int:
-    return (get_score_difference(state, player, scores) * 50) - get_center_proximity(state, player)
-
-
-# ----------- Alpha-Beta search ------------ #
-def cut_off(d: int):
-    return lambda state, depth: depth > d
-
-def h_alpha_beta_search(state: GameStateAbalone, player: PlayerAbalone, cutoff=cut_off(2), h=lambda s: 0) -> tuple[int, Action]:
-     scores = state.get_scores()
-     def max_value(state: GameStateAbalone, alpha: int, beta: int, depth: int) -> tuple[int, Action]:
-        """
-        Max value function.
-
-        Args:
-            state (GameStateAbalone): Current game state
-            alpha (int): Alpha value
-            beta (int): Beta value
-            depth (int): Depth of the search
-
-        Returns:
-            tuple[int, Action]: Tuple with the best value and the best action
-        """
-        if state.is_done():
-            return state.scores[state.next_player.get_id()], None
-        if cutoff(state, depth):
-            return h(state, player, scores), None
-        v, move = -inf, None
-        for a in state.get_possible_actions():
-            v2, _ = min_value(a.get_next_game_state(), alpha, beta, depth+1)
-            if v2 > v:
-                v, move = v2, a
-                alpha = max(alpha, v)
-            if v >= beta:
-                return v, move
-        return v, move
-     
-     def min_value(state: GameStateAbalone, alpha: int, beta: int, depth: int) -> tuple[int, Action]:
-          """
-          Min value function.
-    
-          Args:
-                state (GameStateAbalone): Current game state
-                alpha (int): Alpha value
-                beta (int): Beta value
-                depth (int): Depth of the search
-    
-          Returns:
-                tuple[int, Action]: Tuple with the best value and the best action
-          """
-          if state.is_done():
-            return state.scores[state.next_player.get_id()], None
-          if cutoff(state, depth):
-            return h(state, player, scores), None
-          v, move = inf, None
-          for a in state.get_possible_actions():
-                v2, _ = max_value(a.get_next_game_state(), alpha, beta, depth+1)
-                if v2 < v:
-                 v, move = v2, a
-                 beta = min(beta, v)
-                if v <= alpha:
-                 return v, move
-          return v, move
-     
-     return max_value(state, -inf, +inf, 0)    
 
 class MyPlayer(PlayerAbalone):
     """
@@ -166,11 +38,107 @@ class MyPlayer(PlayerAbalone):
         Returns:
             Action: selected feasible action
         """
+        return self.alphabeta_search(current_state)[1]
+    
+    def cutoff_depth(d):
+        """A cutoff function that searches to depth d."""
+        return lambda current_state, depth: depth > d
+    
 
-        print("Current state: ", current_state)
-        print("self ", self)
+    def getNearCenterScore(self, game_state: GameStateAbalone):
+        other_id = game_state.next_player.get_id()
+        current_rep = game_state.get_rep()
+        board = current_rep.get_env()
+
+        center_offset_0 = [(8,4)]
+        center_offset_1 = [(7,3), (6,4), (7,5), (9,5), (10,4), (9,3)]
+        center_offset_2 = [(6,2), (5,3), (4,4), (5,5), (6,6), (8,6), (10,6), (11,5), (12,4), (11,3), (10,2), (8,2)]
+        center_offset_3 = [(5,1), (4,2), (3,3), (2,4), (3,5), (4,6), (5,7), (7,7), (9,7), (11,7), (12,6), (13,5), (14,4), (13,3), (12,2), (11,1), (9,1), (7,1)]
+        center_offset_4 = [(4,0), (3,1), (2,2), (1,3), (0,4), (1,5), (2,6), (3,7), (4,8), (6,8), (8,8), (10,8), (12,8), (13,7), (14,6), (15,5), (16,4), (15,3), (14,2), (13,1), (12,0), (10,0), (8,0), (6,0)]
+
+        player_pieces = []
+        opponent_pieces = []
+
+        for i, j in list(board.keys()):
+            coordinate = board.get((i, j), None)
+            if coordinate.get_owner_id() == self.id:
+                player_pieces.append((i,j))
+            if coordinate.get_owner_id() == other_id:
+                opponent_pieces.append((i,j))
         
+        center_control_weight = 0 # max = 1, min = 0
 
-        if self.get_remaining_time() < 60:
-            pass
-        return h_alpha_beta_search(current_state, self, h=heuristic)[1]
+        total_weight = 0
+        for position in player_pieces:
+            if position in center_offset_0:
+                total_weight += 1
+            elif position in center_offset_1:
+                total_weight += 0.8
+            elif position in center_offset_2:
+                total_weight += 0.6
+            elif position in center_offset_3:
+                total_weight += 0.3
+            elif position in center_offset_4:
+                total_weight += 0
+        center_control_weight = total_weight / len(player_pieces)
+        return center_control_weight
+    
+    def getScore(self, current_state: GameStateAbalone):
+        other_id = current_state.next_player.get_id()
+        score = current_state.scores[self.id] - current_state.scores[other_id]
+        return score
+
+    def heuristic(self, current_state: GameStateAbalone):
+        return 0
+        score = self.getScore(current_state)
+        center = self.getNearCenterScore(current_state)
+        weightScore = 0.8
+        weightCenter = 0.2
+        return weightScore * score + weightCenter * center
+
+    def alphabeta_search(self, game_state: GameStateAbalone, cutoff=cutoff_depth(2)):
+
+        def max_value(current_state: GameStateAbalone, alpha, beta, depth):
+
+            if current_state.is_done() or cutoff(current_state, depth):
+                return self.heuristic(current_state), None
+
+            v_star = - infinity
+            m_star = None
+
+            possible_actions = list(current_state.get_possible_actions())
+            for possible_action in possible_actions:
+                new_state = possible_action.get_next_game_state()
+                (v, _) = min_value(new_state, alpha, beta, depth + 1)
+                if v > v_star:
+                    v_star = v
+                    m_star = possible_action
+                    alpha = max(alpha, v_star)
+                if v_star >= beta:
+                    return (v_star, m_star)
+            return (v_star, m_star)
+
+        def min_value(current_state: GameStateAbalone, alpha, beta, depth):
+            
+            if current_state.is_done() or cutoff(current_state, depth):
+                return self.heuristic(current_state), None
+
+            v_star = infinity
+            m_star = None
+
+            possible_actions = list(current_state.get_possible_actions())
+            for possible_action in possible_actions:
+                new_state = possible_action.get_next_game_state()
+                (v, _) = max_value(new_state, alpha, beta, depth + 1)
+                if v < v_star:
+                    v_star = v
+                    m_star = possible_action
+                    beta = min(beta, v_star)
+                if v_star <= beta:
+                    return (v_star, m_star)
+            return (v_star, m_star)
+        
+        next_action = max_value(game_state, -infinity, +infinity, 0)
+        return next_action
+
+infinity = math.inf
